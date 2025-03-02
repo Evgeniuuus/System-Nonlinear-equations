89880165a2b08c28b06a620eb320e6d9a5571342
Evgeniuuus <ezhelinskij@bk.ru>
2025-03-02 12:59:59 +0700

    roots find

---
+++ functions.py
@@ -0,0 +1,74 @@
+# Вариант № 3
+# Анатолий 6
+
+import numpy as np
+
+
+
+
+
+def EasyGauss(a: np.array):
+    def div_row(a: np.array, n: int, divider: float):
+        for j in range(len(a[i])):
+            a[n][j] /= divider
+        return a
+
+    def sub_rows(a: np.array, n: int):
+        for i in range(len(a)):
+            k = a[i][n]
+            for j in range(len(a[i])):
+                if i != n:
+                    a[i][j] -= a[n][j] * k
+        return a
+
+    for i in range(len(a)):
+        # print(a)
+        if a[i][i] != 0:
+            a = div_row(a, i, a[i][i])
+            a = sub_rows(a, i)
+    return a
+
+
+def NotSoEasyGauss(a: np.array):
+    tmp = a
+    # s = 1
+    print('матрица:')
+    print(tmp)
+    for i in range(len(tmp)):
+        k = np.argmax(abs(tmp[i:, i])) + i
+        if i != k:
+            tmp[[i, k]] = tmp[[k, i]]
+            # s*=-1
+        # print(tmp,s)
+
+    def div_row(a: np.array, n: int, divider: float):
+        for j in range(len(a[i])):
+            a[n][j] /= divider
+        return a
+
+    def sub_rows(a: np.array, n: int):
+        for i in range(len(a)):
+            k = a[i][n]
+            for j in range(len(a[i])):
+                if i != n:
+                    a[i][j] -= a[n][j] * k
+        return a
+
+    for i in range(len(a)):
+        # print(a)
+        if a[i][i] != 0:
+            a = div_row(a, i, a[i][i])
+            a = sub_rows(a, i)
+    return a
+
+
+def get_answer(a: np.array):
+    answ = []
+    if len(a) == len(a[0]) - 1:
+        for i in range(len(a)):
+            for j in range(len(a[0])):
+                if i == j:
+                    answ.append(a[i][-1] / a[i][j])
+        return answ
+    else:
+        return []
---
+++ main.py
@@ -0,0 +1,114 @@
+import matplotlib.pyplot as plt
+import sympy as sp
+from functions import *
+from random import randint
+
+
+def phi_1(x, y):
+    return np.sign(x) * np.sqrt(4 - y ** 2)
+
+
+def phi_2(x, y):
+    return 3 * x ** 2
+
+
+F_x_y_1 = "x^2 + y^2 - 4 = 0"
+F_x_y_2 = "3*x^2 - y = 0"
+
+print("Функция 1: ", F_x_y_1)
+print("Функция 2: ", F_x_y_2)
+
+x_1 = np.linspace(-2, 2, 1000)
+y_up = np.sqrt(4 - x_1 ** 2)
+y_down = -np.sqrt(4 - x_1 ** 2)
+
+x_2 = np.linspace(-1, 1, 1000)
+y_2 = 3 * x_2 ** 2
+
+root_x = [0.7832125644, -0.7832125644]
+root_y = [1.8402657631, 1.8402657631]
+
+plt.title('График', fontsize=14, fontname='Times New Roman')
+ax = plt.gca()
+ax.set_xlim([-3.5, 3.5])
+ax.set_ylim([-3, 3])
+plt.plot(x_1, y_up, color="red", label="x^2 + y^2 - 4 = 0")
+plt.plot(x_1, y_down, color="red")
+plt.plot(x_2, y_2, '--', color="green", label="3*x^2 - y = 0")
+plt.scatter(root_x, root_y, color='blue', label='Найденные корни')
+
+plt.grid()
+plt.legend()
+plt.show()
+
+print('\n-----------------------Метод простой итерации----------------------')
+
+x_0, y_0 = 0.1, 0.5
+max_iter = 50
+
+# epsilon = np.double(input("Введите Эпсилон: "))
+epsilon = 0.001
+switch = True
+
+for i in range(max_iter):
+
+    x_next = phi_1(x_0, y_0)
+    y_next = phi_2(x_0, y_0)
+
+    if np.isnan(x_next) or np.isnan(y_next):
+        print("Итерации расходятся (NaN)")
+        switch = False
+        break
+
+    if abs(x_next - x_0) < epsilon and abs(y_next - y_0) < epsilon:
+        print("Метод сошелся")
+        break
+
+    x_0, y_0 = x_next, y_next
+
+
+print(f"Решение: x = {x_0:.6f}, y = {y_0:.6f}")
+if not switch:
+    print("Решение ошибочно")
+
+print("\n---------------------------Метод Ньютона----------------------------")
+
+'''
+n = (3, 3)
+A1 = np.array([[1., 2., 3.], [3., 5., 7.], [1., 3., 4.]])
+A2 = np.zeros(n)
+
+A3 = np.zeros(n)
+A4 = np.zeros(n)
+A5 = np.zeros(n)
+f1 = np.array([3, 0, 1])
+f2 = np.zeros(n[0])
+f3 = np.zeros(n[0])
+f4 = np.zeros(n[0])
+f5 = np.zeros(n[0])
+
+for i in range(n[0]):
+    for j in range(n[0]):
+        A2[i][j] = randint(-10, 10)
+        if i == j:
+            A3[i][j] = 1
+        A5[i][j] = 1 / (i + j + 1)
+    f2[i] = randint(-10, 10)
+    f3[i] = randint(-10, 10)
+    f4[i] = randint(-10, 10)
+    f5[i] = randint(-10, 10)
+
+print(A1, f1)
+print(A2, f2)
+print(A3, f3)
+print(A4, f4)
+print(A5, f5)
+
+tmp = np.c_[A5, f5]
+print(tmp)
+
+print(EasyGauss(tmp))
+print(NotSoEasyGauss(np.c_[A5, f5]))
+print('Решение: ', get_answer(NotSoEasyGauss(np.c_[A5, f5])))
+print('Проверка: ', np.dot(A5, get_answer(NotSoEasyGauss(np.c_[A5, f5]))))
+'''
